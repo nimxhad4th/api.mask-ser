c4c08524e430440a18954e1648e166e1cb0a1593
Mikey <105102644+nimxhad4th@users.noreply.github.com>
2022-09-12 11:56:09 +0530

    Create mikey.js

---
+++ mikey.js
@@ -0,0 +1,12 @@
+{ "CREATOR" : "Mikey",
+   "result" : ["https://i.imgur.com/wAbEMAO.mp4",
+"https://i.imgur.com/M5jMPpa.mp4", 
+"https://i.imgur.com/RMnUWfn.mp4",
+"https://i.imgur.com/dzIzWdk.mp4",
+"https://i.imgur.com/u74todR.mp4",
+"https://i.imgur.com/z2o7kiv.mp4",
+"https://i.imgur.com/DtBRik1.mp4",
+"https://i.imgur.com/aQZap1w.mp4",
+"https://i.imgur.com/AEdyp5n.mp4"
+]
+}
